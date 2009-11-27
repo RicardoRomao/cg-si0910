f59201a57ad089bf2f4d3e84e5baaeaf3082b643
nunogomesdesousa <nunogomesdesousa@b2da91ea-d9be-11de-ac08-77837bae5fd2>
2009-11-27 00:45:20 +0000

    Enum ElementType

--- trunk/Trabalho 1/Battleships/src/Elements/Water.java
+++ trunk/Trabalho 1/Battleships/src/Elements/Water.java
@@ -1,7 +1,22 @@
 package Elements;
 
+import java.awt.Point;
+import java.util.Collection;
+
 public class Water implements IElement{
 
     public void draw(){}
+
+    public void drawDamage() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public boolean hit() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Collection<Point> getPoints() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
 }
